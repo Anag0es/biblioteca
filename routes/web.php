f571e926b4c7ff74f1054b737aea873f6da88029
Anag0es <analuisa.goes2004@gmail.com>
2022-02-17 22:24:40 -0300

    Adicionando links e removendo códigos 😎😎

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/consultacervo', function(){
+    return view('consultacervo');
+});
+
+Route::get('/realizaemprestimo', function(){
+    return view('realizaemprestimo');
+});
